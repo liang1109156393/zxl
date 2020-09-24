aa6697b8c3fb1c056389ed059eb346b14ba5135a
zhangxiaoliang <zhangxiaoliang@zcckj.com>
2020-09-24 15:00:52 +0800

    cglib代理

---
+++ JavaGuide-master/code/java/proxy/cglib/RoleDao.java
@@ -0,0 +1,12 @@
+package com.zcckj.taiyi.account.cglib;
+
+/**
+ * @description:
+ * @author: zxl
+ * @create: 2020-09-24 14:52
+ **/
+public class RoleDao {
+    public final void save() {
+        System.out.println("----已经保存数据!----");
+    }
+}
---
+++ JavaGuide-master/code/java/proxy/cglib/Test.java
@@ -0,0 +1,21 @@
+package com.zcckj.taiyi.account.cglib;
+
+/**
+ * @description:
+ * @author: zxl
+ * @create: 2020-09-24 14:53
+ **/
+public class Test {
+    public static void main(String[] args) {
+        //目标对象 cglib 代理无需实现接口
+        //代理的类不能为final,否则报错
+        //目标对象的方法如果为final/static,那么就不会被拦截,即不会执行目标对象额外的业务方法.
+        RoleDao target = new RoleDao();
+
+        //代理对象
+        RoleDao proxy = (RoleDao)new ProxyFactory(target).getProxyInstance();
+
+        //执行代理对象的方法
+        proxy.save();
+    }
+}
