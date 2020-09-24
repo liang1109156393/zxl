3eac312939b912b76e5e0946396443082ed29f77
zhangxiaoliang <zhangxiaoliang@zcckj.com>
2020-09-24 14:51:27 +0800

    jdk代理

---
+++ JavaGuide-master/code/java/JdkProxy/staticProxy/UserDaoProxy.java
@@ -0,0 +1,21 @@
+package com.zcckj.taiyi.account.JdkProxy.staticProxy;
+
+/**
+ * @description:
+ * @author: zxl
+ * @create: 2020-09-24 14:42
+ **/
+public class UserDaoProxy implements IUserDTO {
+    //接收保存目标对象
+    private IUserDTO target;
+
+    public UserDaoProxy(IUserDTO target) {
+        this.target = target;
+    }
+    @Override
+    public void save() {
+        System.out.println("开始事务...");
+        target.save();//执行目标对象的方法
+        System.out.println("提交事务...");
+    }
+}
