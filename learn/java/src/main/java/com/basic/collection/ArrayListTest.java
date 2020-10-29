d1bc3c80c82e81900a218e82117fca0d45ec1ca3
zhangxiaoliang <zhangxiaoliang@zcckj.com>
2020-10-29 17:34:07 +0800

    thread

--- learn/java/src/main/java/com/basic/collection/ArrayListTest.java
+++ learn/java/src/main/java/com/basic/collection/ArrayListTest.java
@@ -1,12 +1,6 @@
 package com.basic.collection;
 
-import com.baomidou.mybatisplus.extension.api.R;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.function.Consumer;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @description:
@@ -15,39 +9,45 @@ import java.util.function.Consumer;
  **/
 public class ArrayListTest {
     /*数组的创建方式:
-    *  String[] string = new String[length];
-    * String[] string = {"1"};
-    * String[] string = new String[]{"1"};
-    *
-    * ArrayList 有序的,可重复的,线程不安全。
-    * 底层数组实现。扩容为原来的1.5倍 会出现fail-fast机制
-    *
-    *
-    *
-    *
-    *
-    * */
+     *  String[] string = new String[length];
+     * String[] string = {"1"};
+     * String[] string = new String[]{"1"};
+     *
+     * ArrayList 有序的,可重复的,线程不安全。
+     * 底层数组实现。扩容为原来的1.5倍 会出现fail-fast机制
+     *
+     *
+     *
+     *
+     *
+     * */
 
-
-
-
-
-
-
-
-
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            list.add(i + "");
+    public static void main(String [] args){
+        SubThread1 subThread1=new SubThread1();
+        subThread1.start();
+        for (int i=0;i<=4;i++){
+            System.out.println(Thread.currentThread().getName()+":"+i);
+            if(i==2){
+                try {
+                    subThread1.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        list.forEach(e->{
-            System.out.println("d");
-        });
-
-
     }
+}
 
-
-
+class SubThread1 extends Thread{
+    @Override
+    public void run(){
+        for (int i=6;i<=10;i++){
+            try {
+                Thread.currentThread().sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName()+":"+i);
+        }
+    }
 }
