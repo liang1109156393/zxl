944416412e11bac17273a33162ad6f80070bd817
zhangxiaoliang <zhangxiaoliang@zcckj.com>
2020-10-29 13:31:05 +0800

    arrayList

--- learn/java/src/main/java/com/basic/collection/ArrayListTest.java
+++ learn/java/src/main/java/com/basic/collection/ArrayListTest.java
@@ -2,6 +2,7 @@ package com.basic.collection;
 
 import com.baomidou.mybatisplus.extension.api.R;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -13,6 +14,28 @@ import java.util.function.Consumer;
  * @create: 2020-10-28 13:46
  **/
 public class ArrayListTest {
+    /*数组的创建方式:
+    *  String[] string = new String[length];
+    * String[] string = {"1"};
+    * String[] string = new String[]{"1"};
+    *
+    * ArrayList 有序的,可重复的,线程不安全。
+    * 底层数组实现。扩容为原来的1.5倍 会出现fail-fast机制
+    *
+    *
+    *
+    *
+    *
+    * */
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
@@ -21,6 +44,7 @@ public class ArrayListTest {
         list.forEach(e->{
             System.out.println("d");
         });
+
 
     }
 
