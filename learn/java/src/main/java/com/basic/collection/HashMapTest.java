2ff04b6d9a2d3b467f7b3177101304daa5a0aabb
zhangxiaoliang <zhangxiaoliang@zcckj.com>
2020-11-02 16:23:11 +0800

    map

--- learn/java/src/main/java/com/basic/collection/HashMapTest.java
+++ learn/java/src/main/java/com/basic/collection/HashMapTest.java
@@ -1,5 +1,7 @@
 package com.basic.collection;
 
+import java.util.*;
+
 /**
  * @description:
  * @author: zxl
@@ -7,6 +9,14 @@ package com.basic.collection;
  **/
 public class HashMapTest {
     public static void main(String[] args) {
+        HashSet<String> strings = new HashSet<>();
+        TreeSet<String> strings1 = new TreeSet<>();
+        LinkedHashSet<String> strings2 = new LinkedHashSet<>();
+
+        HashMap<String, String> stringStringHashMap = new HashMap<>();
+        new TreeMap<String,String>();
+        new LinkedHashMap<String,String>();
+        new Hashtable<String,String>();
 
     }
 }
