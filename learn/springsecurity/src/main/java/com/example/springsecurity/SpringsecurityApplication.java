3e80b0b898c0e9df8f2e20d75fd32e380781c031
zhangxiaoliang <zhangxiaoliang@zcckj.com>
2020-11-04 08:51:42 +0800

    security

---
+++ learn/springsecurity/src/main/java/com/example/springsecurity/SpringsecurityApplication.java
@@ -0,0 +1,21 @@
+package com.example.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityApplication {
+	/* WebSecurityConfigurerAdapter 文件配置类中配置过滤器。
+	*  在执行AbstractAuthenticationProcessingFilter权限认证抽象类时候，执行子类的attemptAuthentication
+	*  返回一个Authentication 是通过AuthenticationManager的authenticate方法返回也就是执行ProviderManager的authenticate方法
+	*  在ProviderManager中执行AuthenticationProvider的authenticate方法实现用户认证
+	*  userDetailsService.loadUserByUsername返回返回一个Authentication
+	*  认证成功或失败执行AbstractAuthenticationProcessingFilter的子类的successfulAuthentication或者unsuccessfulAuthentication
+	*
+	*
+	* */
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityApplication.class, args);
+	}
+
+}
---
+++ learn/springsecurity/src/main/java/com/example/springsecurity/security/LoginFilterProcessingFilter.java
@@ -0,0 +1,30 @@
+package com.example.springsecurity.security;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @description:
+ * @author: zxl
+ * @create: 2020-11-03 15:24
+ **/
+public class LoginFilterProcessingFilter extends AbstractAuthenticationProcessingFilter {
+
+    public LoginFilterProcessingFilter(String defaultFilterProcessesUrl) {
+        super(defaultFilterProcessesUrl);
+    }
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
+        String username = httpServletRequest.getParameter("username");
+        String password = httpServletRequest.getParameter("password");
+        return this.getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(username,password));
+    }
+}
